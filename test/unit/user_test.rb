085612d8db8713343445e1b7183afe409245c377
Jyri Paavilainen <jyri.paavilainen@mac.com>
2013-01-15 13:07:32 +0200

    Added tests for profile name

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -28,12 +28,22 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "a user should have a profile name without spaces" do
-		user = User.new
+		user = User.new(first_name: 'Jyri', last_name: 'Seifer', email: 'jyri.paavilainen@me.com')
+		user.password = user.password_confirmation = 'asdfasdf'
+
 		user.profile_name ="My Profile With Spaces"
 
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
+
+	test "a user can have a correctly formatted profile name" do
+		user = User.new(first_name: 'Jyri', last_name: 'Seifer', email: 'jyri.paavilainen@me.com')
+		user.password = user.password_confirmation = 'asdfasdf'
+
+		user.profile_name = 'jyripaavilainen1'
+		assert user.valid?
 	end
 
 end
