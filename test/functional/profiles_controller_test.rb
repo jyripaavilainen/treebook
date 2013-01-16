25a1ff656af49b5ca70a0b0a0fae77b76971c857
Jyri Paavilainen <jyri.paavilainen@mac.com>
2013-01-16 11:31:39 +0200

    Updated profile page, added scoped status updates

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -12,5 +12,17 @@ class ProfilesControllerTest < ActionController::TestCase
     assert_response :not_found
   end
 
+  test "that variables are assigned on successful profile viewing" do
+    get :show, id: users(:jyri).profile_name
+    assert assigns(:user)
+    assert_not_empty assigns(:statuses)
+  end
+
+  test "only shows the correct user's statuses" do
+    get :show, id: users(:jyri).profile_name
+    assigns(:statuses).each do |status|
+    	assert_equal users(:jyri), status.user
+    end
+  end
 
 end
